f9878a8af25967597ee74bf1fae45b0b15b336dc
luckuslin <lukasl18@nycstudents.net>
2026-04-30 09:24:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 
 class Villain:
-    def __init__(self,name,weapon,money,power,hp):
-        self.name=name
+    def __init__(self,villainname,weapon,money,power,hp):
+        self.villainname=villainname
         self.money=money
         self.power=power
         self.hp=hp
         self.__weapon=weapon
 
     def weaponattack(self,damage):
-        self.__weapon += damage
         print(f"{self.__weapon} has done {damage}")
 
-    def powersummon(self,Kingsley):
-        print(f"{self.name} has summoned {Kingsley[self.name]}")
-
-
+    
 class Monster(Villain): 
     def __init__(self, name, money,power,hp):
-        self.name = name
+        self.__name = name
         self.money = money
         self.power = power
         self.hp = hp
+
+    def summoned(self):
+        super().__init__(villainname="Goyco")
+        print(f"{self.villainname} has summoned {__name__}")
+
     def attack(power, self):
         self.power.append()
         print(f"{self.hp} has %{self.__hp}")
@@ -31,6 +32,6 @@ Kingsley = Monster("Kingsley",1000,"MindlessCrashouts",80)
 Goyco = Villain("Goyco","Textbook",0,"summons",200)
 
 Goyco.weaponattack(50)
-Goyco.powersummon()
+Goyco.powersummon(Kingsley)
 
         
