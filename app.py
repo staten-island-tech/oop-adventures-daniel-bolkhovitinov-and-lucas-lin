2a5abcc4394cbfd97b9fb392007b30a2d2b66f8d
luckuslin <lukasl18@nycstudents.net>
2026-04-30 09:14:22 -0400

    r
    r

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-class Rarity:
-    def __init__(self,common,uncommon,rare,epic,legendary):
-        self.common=common
-        self.uncommon=uncommon
-        self.rare=rare
-        self.epic=epic
-        self.legendary=legendary
-    def damage(self,damage,common,uncommon,rare,epic,legendary):
-        
 
-
-class Villain(Rarity):
-    def __init__(self,name,level,weapon,armor,health):
+class Villain:
+    def __init__(self,name,weapon,money,power,hp):
         self.name=name
-        self.level=level
-        self.weapon=weapon
-        self.armor=armor
-        self.health=health
+        self.money=money
+        self.power=power
+        self.hp=hp
+        self.__weapon=weapon
 
-    def weaponstats(self,weapon,damage):
+    def weaponattack(self,damage):
+        self.__weapon += damage
+        print(f"{self.__weapon} has done {damage}")
+
+    def powersummon(self,Kingsley):
+        print(f"{self.name} has summoned {Kingsley[self.name]}")
+
+
+class Monster(Villain): 
+    def __init__(self, name, money,power,hp):
+        self.name = name
+        self.money = money
+        self.power = power
+        self.hp = hp
+    def attack(power, self):
+        self.power.append()
+        print(f"{self.hp} has %{self.__hp}")
+        
+        
+Kingsley = Monster("Kingsley",1000,"MindlessCrashouts",80)
+
+Goyco = Villain("Goyco","Textbook",0,"summons",200)
+
+Goyco.weaponattack(50)
+Goyco.powersummon()
+
         
