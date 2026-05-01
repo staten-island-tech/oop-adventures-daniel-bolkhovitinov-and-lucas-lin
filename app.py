72530a9e9c7632489bdd035eba364ed5d3c05372
luckuslin <lukasl18@nycstudents.net>
2026-05-01 09:12:09 -0400

    f

--- app.py
+++ app.py
@@ -2,17 +2,31 @@ import random
 class Villain:
     def __init__(self,villainname,weapon,money,power,hp):
         self.villainname=villainname
-        self.money=money
+        self.__money=money
         self.power=power
-        self.hp=hp
+        self.__hp=hp
         self.__weapon=weapon
-        villainname=object
+        
 
     def weaponattack(self,damage):
         print(f"{self.__weapon} has done {damage} damage!")
 
     def summoned(self,__name):
         print(f"{self.villainname} has summoned {__name}")
+
+    def powerattack(self):
+        damage=random.randint(50,100)
+        print(f"{idk['_Monster__name']} with {idk['power']} has done {damage} damage!")
+
+    def gettingattacked(self):
+        attacked = random.randint(20,30)
+        print(f"heroname has done {attacked} damage to {self.villainname}")
+        self.__hp -= attacked
+        print(f"{self.villainname} health is now {self.__hp}!")
+
+        if self.__hp == 0:
+            print(f"{self.villainname} has been defeated.{self.villainname}has dropped{}")
+
 
 class Monster(Villain): 
     def __init__(self, name, money,power,hp):
@@ -21,13 +35,6 @@ class Monster(Villain):
         self.power = power
         self.hp = hp
 
-<<<<<<< Updated upstream
-=======
-    def summoned(self,__name):
-        super().__init__(villainname="Goyco")
-        print(f"{self.villainname} has summoned {__name}")
-
->>>>>>> Stashed changes
     def attack(power, self):
         self.power.append()
         print(f"{self.hp} has %{self.__hp}")
@@ -36,13 +43,10 @@ class Monster(Villain):
 Kingsley = Monster("Kingsley",1000,"MindlessCrashouts",80)
 idk=Kingsley.__dict__
 
-Goyco = Villain("Goyco","Textbook",0,"summons",200)
+Goyco = Villain("Goyco","Textbook",100,"summons",200)
 
 Goyco.weaponattack(50)
-<<<<<<< Updated upstream
 Goyco.summoned(idk['_Monster__name'])
-=======
-Kingsley.summoned(Kingsley)
->>>>>>> Stashed changes
-
+Goyco.powerattack()
+Goyco.gettingattacked()
         
